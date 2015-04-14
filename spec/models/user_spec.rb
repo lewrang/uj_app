c20aadd34c964570216414e591f547d67ff2fdf6
Sabinka <cyzzzowna@gmail.com>
2015-04-14 12:31:03 +0000

    refactoring testing code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,19 @@
 require "rails_helper"
 
 describe User  do
+  before :each do
+    @user = User.new(first_name: "Andy", last_name: "Lindeman")
+  end
+  
   describe "full name" do
     it "returns user full name" do
-      user = User.new(first_name: "Andy", last_name: "Lindeman")
-      expect(user.full_name).to eq "Andy Lindeman"
+      expect(@user.full_name).to eq "Andy Lindeman"
     end
   end
   
   describe "initials" do
     it "returns user initials" do
-      user = User.new(first_name: "Andy", last_name: "Lindeman")
-      expect(user.initials).to eq "AL"
+      expect(@user.initials).to eq "AL"
     end
   end  
 end
