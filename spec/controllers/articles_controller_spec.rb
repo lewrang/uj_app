0aabcf8343627c18b642ae768ef75af3e8d42b33
Sabinka <cyzzzowna@gmail.com>
2015-04-21 12:20:54 +0000

    test show for articles controller

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,14 +1,26 @@
 require "rails_helper"
 
 describe ArticlesController do
+  before :each do
+    @article1 = create(:article)
+    @article2 = create(:article)
+  end
   describe 'index' do
     it 'return home page' do
-      @article1 = create(:article)
-      @article2 = create(:article)
       get :index
       
       expect(response).to have_http_status(:success)
       expect(assigns(:articles).length).to eq 2
     end
   end
+  
+  describe 'show' do
+    it 'return show page' do
+      get :show, {id: @article1.to_param}
+      
+      expect(response).to have_http_status(:success)
+      expect(assigns(:article)).to eq @article1
+    end
+  end
+  
 end
